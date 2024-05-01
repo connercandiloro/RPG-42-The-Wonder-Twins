a7deda4893ccb1b8994d283b059f175d7f0353ed
James <jahsoftwaredev@outlook.com>
2024-05-01 13:33:33 -0700

    more adjustments

--- Thread_Lord.cc
+++ Thread_Lord.cc
@@ -6,19 +6,16 @@
 class Thread_Safety { 
 
 deque<char> deck;
+mutex lock_deque;
 
 public: 
 Thread_Safety();
 ~Thread_Safety();
 
-mutex lock_deque;
-
 void  in_char(const char &c) {
 lock_guard LG(lock_deque);
-if(c == 'w') deck.push_back('w');
-else if(c == 'a') deck.push_back('a');
-else if(c == 'd') deck.push_back('d');
-else if(c == 's') deck.push_back('s');
+if(c == 'w' || c ==  'a' || c == 'd' || c == 's' ) deck.push_back(c);
+else if(c == KEY_UP || c == KEY_DOWN || c == KEY_UP || c == KEY_LEFT) deck.push_back(c);
 else throw invalid_arguement("invalid movement");
 };
 
