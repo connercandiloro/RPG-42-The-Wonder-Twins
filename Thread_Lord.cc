19507d0064983b8565b27645098a74316e29114e
James <jahsoftwaredev@outlook.com>
2024-04-30 23:07:50 -0700

    fixed minor mistakes

--- Thread_Lord.cc
+++ Thread_Lord.cc
@@ -3,26 +3,27 @@
 #include <deque>
 #include <threads>
 
-struct Thread_Safety { 
+class Thread_Safety { 
 
-Thread_Safety();
-~Thread_Safety();
-	
 deque<char> deck;
 
+public: 
+Thread_Safety();
+~Thread_Safety();
+
+mutex lock_deque;
+
 void  in_char(const char &c) {
-static mutex move_deq_mutex;
-lock_guard LG(move_deq_mutex);
-if(c == 'w') move_deq.push_back('w');
-else if(c == 'a') move_deq.push_back('a');
-else if(c == 'd') move_deq.push_back('d');
-else if(c == 's') move_deq.push_back('s');
+lock_guard LG(lock_deque);
+if(c == 'w') deck.push_back('w');
+else if(c == 'a') deck.push_back('a');
+else if(c == 'd') deck.push_back('d');
+else if(c == 's') deck.push_back('s');
 else throw invalid_arguement("invalid movement");
 };
 
 char out_char(){
-static mutex interact_deq_mutex;
-lock_guard LG(move_deq_mutex);
+lock_guard LG(lock_deque);
 if(deck.size() == 0) throw invalid_arguement("no movement in the deque");
 char temp = deck.front();
 deck.pop_front();
--- Thread_Lord.h
+++ Thread_Lord.h
@@ -1,12 +1,11 @@
 #pragma once
-enum MOVE{UP, DOWN, LEFT, RIGHT};
 
 struct Thread_Safety {
 
 Thread_Safety();
 ~Thread_Safety();
 
-void  in_char(const MOVE  &c);
+void  in_char(const char  &c);
 char  out_char(); 
 }
 
