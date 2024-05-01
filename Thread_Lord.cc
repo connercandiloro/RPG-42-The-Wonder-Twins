99e37e5f69773a9e12fcfee36039c8722eeaff47
James <jahsoftwaredev@outlook.com>
2024-05-01 13:37:40 -0700

    converted the char into ints for input for curselord

--- Thread_Lord.cc
+++ Thread_Lord.cc
@@ -5,21 +5,21 @@
 
 class Thread_Safety { 
 
-deque<char> deck;
+deque<int> deck;
 mutex lock_deque;
 
 public: 
 Thread_Safety();
 ~Thread_Safety();
 
-void  in_char(const char &c) {
+void  in_char(int c) {
 lock_guard LG(lock_deque);
 if(c == 'w' || c ==  'a' || c == 'd' || c == 's' ) deck.push_back(c);
 else if(c == KEY_UP || c == KEY_DOWN || c == KEY_UP || c == KEY_LEFT) deck.push_back(c);
 else throw invalid_arguement("invalid movement");
 };
 
-char out_char(){
+int  out_char(){
 lock_guard LG(lock_deque);
 if(deck.size() == 0) throw invalid_arguement("no movement in the deque");
 char temp = deck.front();
