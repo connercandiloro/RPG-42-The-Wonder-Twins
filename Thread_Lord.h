fdda55422370c2e6c56ba9c6fa78f7650b17354b
James <jahsoftwaredev@outlook.com>
2024-04-30 22:42:02 -0700

    adjusted code to take in only chars to be thread safe

--- Thread_Lord.h
+++ Thread_Lord.h
@@ -1,18 +1,12 @@
 #pragma once
 enum MOVE{UP, DOWN, LEFT, RIGHT};
-enum INTERACT{FIGHT, SPEAK, NEGOTIATE};
-
 
 struct Thread_Safety {
-
 
 Thread_Safety();
 ~Thread_Safety();
 
-
-void  in_movement(const MOVE  &c);
-void  in_interaction(const interact &s); 
-char out_movement();
-string out_interaction();
-
+void  in_char(const MOVE  &c);
+char  out_char(); 
 }
+
