5fe4fce04be1a6dcc9eef122a6db511557a0698c
James <jahsoftwaredev@outlook.com>
2024-05-04 10:56:54 -0700

    back to chars...

--- Thread_Lord.cc
+++ Thread_Lord.cc
@@ -5,14 +5,14 @@
 
 class Thread_Safety { 
 
-deque<int> deck;
+deque<char> deck;
 mutex lock_deque;
 
 public: 
 Thread_Safety();
 ~Thread_Safety();
 
-void in_char(int c) {
+void in_char(const char c) {
 lock_guard LG(lock_deque);
 deck.push_back(c);
 };
@@ -20,7 +20,7 @@ deck.push_back(c);
 int  out_char(){
 lock_guard LG(lock_deque);
 if(deck.size() == 0) throw invalid_arguement("no movement in the deque");
-int temp = deck.front();
+char temp = deck.front();
 deck.pop_front();
 return temp;
 };
