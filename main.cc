19ccd2be2e487936fcec54f4d70a72fd9fc462fa
James <jahsoftwaredev@outlook.com>
2024-05-05 23:09:41 -0700

    same code

--- main.cc
+++ main.cc
@@ -3,54 +3,52 @@
 #include<thread>
 using namespace std;
 
-void inputsafety(Thread_Safety& threadSafety){
-	char input;
-	cin>>input;
-while(true){
-		if(!cin) break;
-		threadSafety.in_char(input);
-	//	cout<< threadSafety.out_char()<< endl;
-}
-}
-int main() {
-    //starts ncurses mode
-    initscr();
-    noecho();
-    start_color();
 
-    //makes main game window
+
+int main() {
+	//starts ncurses mode
+	initscr();
+	noecho();
+	start_color();
+
+	//makes main game window
 	WINDOW* mainwin = newwin(25, 87, 0, 0);
 	wborder(mainwin, (int)'|', (int)'|', (int)'-', (int)'-', (int)'+', (int)'+', (int)'+', (int)'+');
-    keypad(stdscr, true);
+	keypad(stdscr, true);
 	refresh();
 
-    //makes new player with parameters (game window, yPos, xPos, player character)
+	//makes new player with parameters (game window, yPos, xPos, player character)
 	Player* p = new Player(mainwin, 11, 44, 'G');
-    p->loadmap();
-    wrefresh(mainwin);
+	p->loadmap();
+	wrefresh(mainwin);
 
-	Thread_Safety threadSafety;
+	Thread_Safety TS;
 
-	thread input(inputsafety, ref(threadSafety));
+	int choice = 0;
+	//main game loop
+	cutscene_check(p);
+	do {
+		p->display(); //updates player position
+		if (cutscene_check(p)) {
+			break; //breaks game loop if final cutscene is played
+		}
+		//	p->getinput();
+		// p->getinput(threadSafety.out_char());									//		choice = p->getinput(); //waits for player input
 
-	int choice;
-    //main game loop
-    cutscene_check(p);
-    do {
-        p->display(); //updates player position
-        if (cutscene_check(p)) {
-            break; //breaks game loop if final cutscene is played
-        }
-	//	p->getinput();
-   // p->getinput(threadSafety.out_char());
-choice= threadSafety.out_char();
-//		choice = p->getinput(); //waits for player input
+	} while (choice != 'z'); //game quits whenever player inputs 'z'
+	while(true) { 
+	char c;
+	cin >> c;
+	char out;
+	thread t1(TS.in_char(), &c);
+	thread t2(TS.out_char(), &out);
+	t1.join();
+	t2.join();
 
-    } while (choice != 'z'); //game quits whenever player inputs 'z'
-    input.join();
-          while (choice == 'z');
-    //game quits whenever player inputs 'z'
-    endwin();
+	p->getinput(out);
+	}
+	//game quits whenever player inputs 'z'
+	endwin();
 
-    return 0;
+	return 0;
 }
